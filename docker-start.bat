a6cd0592fa7ecc29e0a6a2c304761ebf5c1ddcd9
nop <575065955@qq.com>
2021-04-19 16:12:14 +0800

    add vue / lodash template sample

--- docker-start.bat
+++ docker-start.bat
@@ -12,3 +12,4 @@ IF "%WEB_PORT%" == "" set WEB_PORT=3000
 call wslpath.bat "dist" WEB_PATH 2>NUL
 
 echo docker run -p %WEB_PORT%:3000 -td --rm -v "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
+docker run -p %WEB_PORT%:3000 -td --rm -v "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
