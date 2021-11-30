97928e25707b0b6f2ead5ca530e2ceec1d438b78
wuxue107 <575065955@qq.com>
2021-12-01 03:46:52 +0800

    down static codemirror 5.64.0

--- docker-start.bat
+++ docker-start.bat
@@ -11,8 +11,8 @@ IF "%WEB_PORT%" == "" set WEB_PORT=3000
 
 call wslpath.bat "dist" WEB_PATH 2>NUL
 
-echo docker run -p %WEB_PORT%:3000 -td --rm -v "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
-docker run -p %WEB_PORT%:3000 -td --rm -v "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
+echo docker run --cpus="0.7" -p %WEB_PORT%:3000 -td --rm -v  "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
+docker run --cpus="0.7" -p %WEB_PORT%:3000 -td --rm -v "%WEB_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server:%IMAGE_TAG%
 
 echo LISTEN : %WEB_PORT%
 echo WEB_ROOT : %WEB_PATH%
--- docker-start.sh
+++ docker-start.sh
@@ -11,8 +11,8 @@ IMAGE_TAG=$2
 WEB_PATH=${PWD}/dist
 [ -d "${WEB_PATH}" ] || mkdir "${WEB_PATH}"
 
-echo docker run -p ${WEB_PORT}:3000 -td --rm -v ${WEB_PATH}:/screenshot-api-server/public --name=screenshot-api-server wuxue107/screenshot-api-server:${IMAGE_TAG}
-docker run -p ${WEB_PORT}:3000 -td --rm -v ${WEB_PATH}:/screenshot-api-server/public --name=screenshot-api-server wuxue107/screenshot-api-server:${IMAGE_TAG}
+echo docker run --cpus="0.7" -p ${WEB_PORT}:3000 -td --rm -v ${WEB_PATH}:/screenshot-api-server/public --name=screenshot-api-server wuxue107/screenshot-api-server:${IMAGE_TAG}
+docker run --cpus="0.7" -p ${WEB_PORT}:3000 -td --rm -v ${WEB_PATH}:/screenshot-api-server/public --name=screenshot-api-server wuxue107/screenshot-api-server:${IMAGE_TAG}
 
 echo LISTEN : ${WEB_PORT}
 echo WEB_ROOT : ${WEB_PATH}
